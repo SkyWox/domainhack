9882a1ad13dd12a7c591b418df8896760165eaef
Skylar Wilcox <lenin1916@gmail.com>
2017-12-14 10:47:10 -0800

    security features

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').load()
+  require('dotenv').load()
 }
 
 var app = express()
+
+app.disable('x-powered-by') //hide server tech
+//set security headers
+app.use(function(req, res, next) {
+  res.header('X-XSS-Protection', '1; mode=block')
+  res.header('X-Frame-Options', 'deny')
+  res.header('X-Content-Type-Options', 'nosniff')
+  next()
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -31,20 +40,20 @@ console.log(`Express server listening on ${port}`)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found')
-	err.status = 404
-	next(err)
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
 })
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message
-	res.locals.error = req.app.get('env') === 'development' ? err : {}
-	console.log(err)
-	// render the error page
-	res.status(err.status || 500)
-	res.render('error')
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  console.log(err)
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
 module.exports = app
--- routes/whois.js
+++ routes/whois.js
@@ -4,42 +4,53 @@ const axios = require('axios')
 
 router.get('/', function(req, res, next) {
 	dom = req.query.domain
+	var maxAttempts = 4
+	var attempts = 0
 	callWhois(dom)
 	//uncomment below for "offline" testing
 	//res.json({available : true})
 
 	function callWhois(domain) {
-		axios({
-			method: 'get',
-			url: 'https://whois-v0.p.mashape.com/check?domain=' + domain,
-			timeout: req.socket._idleTimeout,
-			headers: {
-				'X-Mashape-Key': process.env.WHOIS_MASHAPE_KEY
-			}
-		})
-			.then(resp => {
-				console.log(domain + ' is ' + resp.data.available)
-				res.send(resp.data)
-			})
-			.catch(error => {
-				console.log(error)
-				if (error.code === 'ECONNABORTED') {
-					console.log('request-defined timeout on ' + domain + ' restarting')
-					//process.nextTick(() => callWhois(domain))
-					res.send({ available: 'bad' }).status(444)
-				} else if (error.response.status === 404) {
-					console.log('invalid domain ' + domain)
-					res.send({ available: 'bad' })
-				} else if (error.response.status === 504) {
-					console.log('timeout on ' + domain + ' restarting')
-					//try again. don't send back a res so that the client waits
-					//process.nextTick(() => callWhois(domain))
-					res.send({ available: 'bad' })
-				} else {
-					console.log(error)
-					res.send({ available: 'bad' })
+		while (attempts <= maxAttempts) {
+			attempts++
+			axios({
+				method: 'get',
+				url: 'https://whois-v0.p.mashape.com/check?domain=' + domain,
+				timeout: req.socket._idleTimeout,
+				headers: {
+					'X-Mashape-Key': process.env.WHOIS_MASHAPE_KEY
 				}
 			})
+				.then(resp => {
+					console.log(domain + ' is ' + resp.data.available)
+					res.send(resp.data)
+				})
+				.catch(error => {
+					console.log('error area')
+					console.log(error)
+					if (error.code === 'ECONNABORTED') {
+						console.log(
+							'request-defined timeout on ' +
+								domain +
+								' restarting for attempt #' +
+								(attempts + 1)
+						)
+						//process.nextTick(() => callWhois(domain))
+						res.send({ available: 'bad' }).status(444)
+					} else if (error.response.status === 404) {
+						console.log('invalid domain ' + domain)
+						res.send({ available: 'bad' }).status(404)
+					} else if (error.response.status === 504) {
+						console.log('timeout on ' + domain + ' restarting')
+						//try again. don't send back a res so that the client waits
+						//process.nextTick(() => callWhois(domain))
+						res.send({ available: 'bad' }).status(504)
+					} else {
+						console.log(error)
+						res.send({ available: 'bad' })
+					}
+				})
+		}
 	}
 })
 

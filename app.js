5e45a999e6de5dfdf168c29331a7f3dc0fcdf3c7
Skylar Wilcox <lenin1916@gmail.com>
2018-02-06 13:00:49 -0800

    remove jade

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ app.use(function(req, res, next) {
   next()
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
